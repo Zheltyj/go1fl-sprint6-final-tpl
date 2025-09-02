047f9d15ff1bcbcd5a4c5471847eb779523458dc
Zheltyj <itry@yandex.ru>
2025-09-03 00:55:17 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Zheltyj/go1fl-sprint6-final-tpl/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "server: ", log.LstdFlags|log.Lshortfile)
+	serv := server.NewServer(logger)
+
+	if err := serv.HttpServer.ListenAndServe(); err != nil {
+		logger.Fatalf("Server error: %v", err)
+	}
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,62 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Zheltyj/go1fl-sprint6-final-tpl/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("Unsupported method %s", r.Method), http.StatusInternalServerError)
+		return
+	}
+	data, err := os.ReadFile("../index.html")
+	if err != nil {
+		http.Error(w, "Read error index.html", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, fmt.Sprintf("Unsupported method %s", r.Method), http.StatusInternalServerError)
+		return
+	}
+
+	file, _, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Error receiving file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "File read error", http.StatusInternalServerError)
+		return
+	}
+
+	convertedData := service.ConvertString(string(data))
+
+	fileName := fmt.Sprintf("../%s%s", time.Now().UTC().Format("2006-01-02_15-04-05"), filepath.Ext("output.txt"))
+
+	err = os.WriteFile(fileName, []byte(convertedData), 0755)
+	if err != nil {
+		http.Error(w, "File write error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(convertedData))
+}
